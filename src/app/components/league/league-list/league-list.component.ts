d3120fac6c9db23de9d303e564187af112b3ac6e
Rulo Alonso <r.alonso.lopez@gmail.com>
2018-10-12 19:47:33 +0200

    feat: leagues separation

--- src/app/components/league/league-list/league-list.component.ts
+++ src/app/components/league/league-list/league-list.component.ts
@@ -1,6 +1,7 @@
 import { LeagueService, } from './../../../shared/services/league.service';
 import { Component, OnInit } from '@angular/core';
 import { League } from '../../../shared/models/league.model';
+import { SessionService } from 'src/app/shared/services/session.service';
 
 @Component({
   selector: 'app-league-list',
@@ -8,17 +9,27 @@ import { League } from '../../../shared/models/league.model';
   styleUrls: ['./league-list.component.css']
 })
 export class LeagueListComponent implements OnInit {
-  leagues: Array<League> = [];
+  userLeagues: Array<League> = [];
+  otherLeagues: Array<League> = [];
 
-  constructor(private leagueService: LeagueService) { }
+  constructor(
+    private leagueService: LeagueService,
+    private sessionService: SessionService) { }
 
   ngOnInit() {
     this.leagueService.list()
       .subscribe(
         (leagues: Array<League>) => {
-          this.leagues = leagues;
+          this.userLeagues = leagues.filter(league => league.users.includes(this.sessionService.user.id));
+          this.otherLeagues = leagues.filter(league => !league.users.includes(this.sessionService.user.id));
+          console.log(this.sessionService.user);
+          console.log(leagues);
         }
       );
+  }
+
+  createLeague() {
+    
   }
 
 }
